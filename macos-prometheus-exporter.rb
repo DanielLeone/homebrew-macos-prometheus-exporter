ba0ed0a9c89965e6b0105ff7c28c8e607d8ecbc1
Daniel Leone <mail.dleone@gmail.com>
2022-03-18 16:22:07 +0800

    updated formula

--- macos-prometheus-exporter.rb
+++ macos-prometheus-exporter.rb
@@ -1,18 +1,26 @@
 class MacosPrometheusExporter < Formula
   desc "A MacOS WiFi and power metrics Prometheus exporter"
-  homepage ""
-  url "https://github.com/DanielLeone/macos-prometheus-exporter/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "d038798e952f893c77ad1f5af8cf75f4f5f5e40cd2b142911036edbca4a6e665"
+  homepage "https://github.com/DanielLeone/macos-prometheus-exporter"
+  url "https://github.com/DanielLeone/macos-prometheus-exporter/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "fbbe12ed66cdfdb3333e9bc2c33acc463e27cb33c79ae8cc948d52c16ae7b11e"
   license "Apache-2.0"
 
+  depends_on "go" => :build
+
   service do
-    run bin / "macos_prometheus_exporter"
+    run "#{bin}/macos_prometheus_exporter"
     keep_alive true
-    log_path "/usr/local/var/log/macos_prometheus_exporter.log"
-    error_log_path "/usr/local/var/log/macos_prometheus_exporter.err.log"
+    error_log_path "#{var}/log/macos_prometheus_exporter.log"
+    log_path "#{var}/log/macos_prometheus_exporter.err.log"
+  end
+
+  def caveats
+    <<~EOS
+      The launchd service runs on http://127.0.0.1:9003. Logs are located at #{var}/log/macos_prometheus_exporter.log
+    EOS
   end
 
   def install
-    bin.install "main" => "macos_prometheus_exporter"
+    system "./build.sh", "#{bin}/macos_prometheus_exporter"
   end
 end
